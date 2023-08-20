223aa9b32441ef371e6f0d92668731a4f6382b22
mad0g4 <northbankplay@gmail.com>
2023-08-20 17:39:16 +0800

    nor

--- omniisaacgymenvs/tasks/unitree_a1.py
+++ omniisaacgymenvs/tasks/unitree_a1.py
@@ -196,9 +196,9 @@ class UnitreeA1StandTask(RLTask):
         # dof_vel = torch_rand_float(-0.1, 0.1, (num_resets, self._unitree_a1s.num_dof), device=self._device)
         dof_pos = self.default_dof_pos.repeat(num_resets, 1)
         if self.is_sample_init_state:
-            dof_pos[:, :4] = torch_rand_float(self.dof_pos_limit[0, 0], self.dof_pos_limit[0, 1], (self.num_envs, 4), device=self.device)
-            dof_pos[:, 4:8] = torch_rand_float(self.dof_pos_limit[4, 0], self.dof_pos_limit[4, 1], (self.num_envs, 4), device=self.device)
-            dof_pos[:, 8:12] = torch_rand_float(self.dof_pos_limit[8, 0], self.dof_pos_limit[8, 1], (self.num_envs, 4), device=self.device)
+            dof_pos[:, :4] = torch_rand_float(self.dof_pos_limit[0, 0], self.dof_pos_limit[0, 1], (num_resets, 4), device=self.device)
+            dof_pos[:, 4:8] = torch_rand_float(self.dof_pos_limit[4, 0], self.dof_pos_limit[4, 1], (num_resets, 4), device=self.device)
+            dof_pos[:, 8:12] = torch_rand_float(self.dof_pos_limit[8, 0], self.dof_pos_limit[8, 1], (num_resets, 4), device=self.device)
         dof_vel = torch.zeros((self.num_envs, 12), dtype=torch.float, device=self._device, requires_grad=False)
 
         self.current_targets[env_ids] = dof_pos[:]
