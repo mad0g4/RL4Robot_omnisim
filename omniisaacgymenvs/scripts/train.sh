b6988ff249dfa3a6db4f00758f5fbb3068eeba9e
mad0g4 <northbankplay@gmail.com>
2023-08-21 09:54:54 +0800

    nor

--- omniisaacgymenvs/scripts/train.sh
+++ omniisaacgymenvs/scripts/train.sh
@@ -1,7 +1,7 @@
 python rlgames_train.py headless=True device_id=7 rl_device=cuda:7 task=UnitreeA1Stand
 python rlgames_train.py headless=True task=UnitreeA1Stand
 
-python rlgames_train.py headless=True task=UnitreeA1Stand
+python rlgames_train.py headless=True task=UnitreeA1Stand max_iterations=100000
 
 python rlgames_train.py task=UnitreeA1Stand checkpoint=/home/mk/Desktop/code/RL4Robot_omnisim/omniisaacgymenvs/scripts/runs/UnitreeA1Stand/nn/UnitreeA1Stand.pth test=True num_envs=4
 
