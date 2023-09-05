32e978bd23befd740792b3c476bc2704016b4297
mad0g4 <northbankplay@gmail.com>
2023-09-05 11:37:52 +0800

    qq

--- omniisaacgymenvs/scripts/train.sh
+++ omniisaacgymenvs/scripts/train.sh
@@ -7,5 +7,10 @@ python rlgames_train.py headless=True task=UnitreeA1Stand max_iterations=100000 
 python rlgames_train.py task=UnitreeA1Stand checkpoint=/home/mk/Desktop/code/RL4Robot_omnisim/omniisaacgymenvs/scripts/runs/UnitreeA1Stand/nn/UnitreeA1Stand.pth test=True num_envs=4 init_from_prepared_state_data=False push_robots=True
 python rlgames_train.py task=UnitreeA1Stand checkpoint=/home/mk/Desktop/code/RL4Robot_omnisim/omniisaacgymenvs/scripts/runs/UnitreeA1Stand/nn/UnitreeA1Stand.pth test=True num_envs=4 init_from_prepared_state_data=True push_robots=True
 
+
+python rlgames_train.py task=UnitreeA1Stand checkpoint=/home/mk/Desktop/code/RL4Robot_omnisim/omniisaacgymenvs/scripts/runs/UnitreeA1Stand/nn/UnitreeA1Stand.pth test=True num_envs=4 init_from_prepared_state_data=False push_robots=True dummy_action=True
+python rlgames_train.py task=UnitreeA1Stand checkpoint=/home/mk/Desktop/code/RL4Robot_omnisim/omniisaacgymenvs/scripts/runs/UnitreeA1Stand/nn/UnitreeA1Stand.pth test=True num_envs=4 init_from_prepared_state_data=True push_robots=True dummy_action=True
+
+
 # sample prepared state data
 python rlgames_train.py task=UnitreeA1Stand checkpoint=/home/mk/Desktop/code/RL4Robot_omnisim/omniisaacgymenvs/scripts/runs/UnitreeA1Stand/nn/UnitreeA1Stand.pth test=True num_envs=4096 headless=True games_num=100000
